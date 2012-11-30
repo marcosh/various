1a4b855f16537ca36174c7eeef1d14fd97d22cc6
Marco Perone <marco_perone@libero.it>
2012-11-30 17:52:49 +0100

    css e logo
    inseriti css e logo

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<html>
-Ciao!
+<!doctype html>
+<html lang="it">	
+	<head>
+		<title>Chocosite</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<link rel="shortcut icon" href="sosuke.jpg" />
+	</head>
+	<body>
+		<h1>
+			Ciao!
+		</h1>
+	</body>
 <html/>
